271c17d30094179c7ecfe83be6a9b883eb825526
FlorianMMI <florian.bounissou@etu.unlim.fr>
2024-10-25 06:29:41 +0200

    fix et fin de l'itération 7

--- client/src/signin.js
+++ client/src/signin.js
@@ -11,10 +11,18 @@ let handler_sub = async function(ev) {
     let password = data.get('password');
     console.log(password);
     let ok = await UserData.signin(email, password);
-    console.log(ok);
+    
     if (ok) {
-        window.location.href = 'index.html';
         localStorage.setItem('user', JSON.stringify(ok));
+        let temp = JSON.parse(localStorage.getItem('user'));
+        let dataClient = { role: temp.role };
+        if (dataClient.role === 'client') {
+            window.close();
+        }
+        else if (dataClient.role === 'admin') {
+            window.open('../backoffice/index.html', '_blank');
+        }
+        
     }
     else {
         alert("Erreur lors de la connexion");
